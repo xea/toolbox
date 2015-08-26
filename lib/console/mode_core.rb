f1943ca19a8a185853f4a52afd4feb7b4c7c07e6
Alex Pecsi <alex.pecsi@gmail.com>
2015-08-26 20:00:54 +0100

    mode changes?

--- lib/console/mode_core.rb
+++ lib/console/mode_core.rb
@@ -5,9 +5,13 @@ class ModeCore < BaseMode
     mode_id :core
 
     register_command(:list_features, "features", "List features")
+    register_command(:list_services, "services", "List services")
 
     def list_features(framework)
-        p framework
-        puts "blabla"
+        framework.find_all.map { |descriptor| descriptor.features - [ :service ] }.each { |feature| p feature }
+    end
+
+    def list_services(framework)
+        framework.find_all.each { |descriptor| puts "#{descriptor.service.service_id} #{descriptor.service.state}" }
     end
 end
