45a498094b8e3ef0f482c30167a0f063540ede70
Alex Pecsi <alex.pecsi@gmail.com>
2015-10-01 15:01:07 +0100

    simple table generation is now working (?)

--- lib/console/table.rb
+++ lib/console/table.rb
@@ -1,14 +1,35 @@
 
 class PrinTable
+
     def print_table(headers, data, style = nil)
+        case style
+        when :raw
+            print_raw_table(headers, data)
+        when :simple
+            print_simple_table(headers, data)
+        when :db
+            print_db_table(headers, data)
+        else
+            print_simple_table(headers, data)
+        end
     end
 
     def print_simple_table(headers, data)
-        field_count = [ max_field_count(headers), max_field_count(data) ].max
+
+        def print_simple_row(row, lengths)
+            fmt = lengths.map { |l| "%#{l}s" }.join(" ")
+            frow = (0...lengths.length).map { |i| row[i] || "" }
+            fmt % frow
+        end
+
+        # if headers are empty or not specified, we leave them out
+        local_data = ((headers.nil? or headers.length == 0) ? [] : [headers]) + data
+        field_count = max_field_count(data)
 
         if field_count > 0
-            (0..field_count).each do |field_idx|
-            end
+            field_lengths = (0...field_count).map { |field_idx| max_field_length(local_data, field_idx) }
+
+            local_data.map { |row| print_simple_row(row, field_lengths.map { |x| x * -1 }).gsub(/\s*$/, "")  }
         else
             []
         end
@@ -25,15 +46,24 @@ class PrinTable
         result
     end
 
-    # headers = [], data = [ [] ]
     def print_db_table(headers, data)
+        field_count = [ max_field_count(headers), max_field_count(data) ].max
+
+        if field_count > 0
+        else
+            []
+        end
     end
 
     def max_field_count(data)
         if data.nil? or !data.kind_of?(Array) 
             0
         else
-            data.collect { |row| row.length }.max || 0
+            data.collect { |row| (row || []).length }.max || 0
         end
+    end
+
+    def max_field_length(data, idx)
+        data.map { |row| row[idx].to_s.length }.max || 0
     end
 end
--- spec/console/table_spec.rb
+++ spec/console/table_spec.rb
@@ -8,16 +8,30 @@ RSpec.describe PrinTable do
 
     context '#print_simple_table' do
         it 'should omit headers if not present' do
+            headers, data = [nil, [[1, 2], [3, 4]]]
+            expect(@printable.print_simple_table(headers, data)).to eq([ "1 2", "3 4" ])
+            headers, data = [[], [[1, 2], [3, 4]]]
+            expect(@printable.print_simple_table(headers, data)).to eq([ "1 2", "3 4" ])
+        end
+
+        it 'should generate a well idented table' do
+            headers, data = [ [ "Key", "Value" ], [ 
+                [ "fruit", "apple" ], [ "car", "porsche" ]
+            ] ]
+            expect(@printable.print_simple_table(headers, data)).to eq([ "Key   Value", "fruit apple", "car   porsche" ])
         end
     end
 
     context '#print_raw_table' do
         it 'should omit headers if not present' do
             expect(@printable.print_raw_table(nil, [])).to eq([])
-            #expect(@printable.print_raw_table(nil, [ "a" ])).to eq(["a"])
             expect(@printable.print_raw_table(nil, [ [] ])).to eq([])
             expect(@printable.print_raw_table(nil, [ [ "a" ] ])).to eq([ "a" ])
             expect(@printable.print_raw_table(nil, [ [ "a", "b" ] ])).to eq([ "a b" ])
+            tabledata = [
+                [ "alma", "korte" ], [ "repa", "cukor" ]
+            ]
+            expect(@printable.print_raw_table(nil, tabledata)).to eq(["alma korte", "repa cukor"])
         end
     end
 
@@ -33,6 +47,16 @@ RSpec.describe PrinTable do
             expect(@printable.max_field_count([[1], [2]])).to eq(1)
             expect(@printable.max_field_count([[1], [2, 3]])).to eq(2)
             expect(@printable.max_field_count([[1, 1, 2], []])).to eq(3)
+        end
+    end
+
+    context '#max_field_length' do
+        it 'should return the maximum length of fields' do
+            expect(@printable.max_field_length([[]], 0)).to eq(0)
+            expect(@printable.max_field_length([["1"]], 0)).to eq(1)
+            expect(@printable.max_field_length([["1"]], 1)).to eq(0)
+            expect(@printable.max_field_length([["123", "1234"], ["1", "123"]], 0)).to eq(3)
+            expect(@printable.max_field_length([["1"], ["123", "1"]], 0)).to eq(3)
         end
     end
 end
