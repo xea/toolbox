c187edfa961f2cc4dae3626744e897ca76de445b
Alex Pecsi <alex.pecsi@gmail.com>
2016-02-25 11:06:53 +0000

    Improved service discovery dependency resolution

--- lib/service/discovery.rb
+++ lib/service/discovery.rb
@@ -50,8 +50,31 @@ class DiscoveryService < Service
     end
 
     def scan_packages
-        scan_new_packages
-        scan_removed_packages
+        new_pkgs = scan_new_packages
+        old_pkgs = scan_removed_packages
+
+        def step_install(pkgs)
+            clear_new_pkgs = pkgs.find_all { |pkg| pkg[:descriptor]['dependencies'].nil? or pkg[:descriptor]['dependencies'].empty? }
+            dep_new_pkgs = pkgs - clear_new_pkgs
+
+            # First, load packages without dependencies
+            clear_new_pkgs.each do |pkg|
+                load_package pkg[:id], pkg[:descriptor]
+            end
+
+            # Second, try loading naively
+            unresolved_pkgs = dep_new_pkgs.find_all do |pkg|
+                load_package pkg[:id], pkg[:descriptor]
+            end
+
+            # If there are still unresolved packages, we'll try loading them again
+            # in the next iteration.
+
+            # TODO building a dependency tree would be great
+        end
+
+        step_install(new_pkgs)
+
         @logger.debug "Scan finished"
     end
 
@@ -59,11 +82,15 @@ class DiscoveryService < Service
         @logger.debug "Scanning new packages in directory #{@watch_dir}"
 
         if File.exist? @watch_dir and File.directory? @watch_dir
-            (Dir.entries(@watch_dir) - [ ".", ".." ]).each do |entry|
+            (Dir.entries(@watch_dir) - [ ".", ".." ]).map do |entry|
                 if File.directory? "#{@watch_dir}/#{@entry}"
                     scan_package entry
+                else
+                    nil
                 end
             end
+        else
+            []
         end
     end
 
@@ -74,6 +101,9 @@ class DiscoveryService < Service
             removed_entries = @package_db[:installed].keys - Dir.entries(@watch_dir)
             @logger.debug "Removed packages: #{removed_entries}"
             # TODO actually remove packages
+            removed_entries
+        else
+            []
         end
     end
 
@@ -89,12 +119,15 @@ class DiscoveryService < Service
             descriptor = Psych.load_file package_descriptor
 
             if package_valid? descriptor
-                load_package package_name, descriptor
+                { id: package_name, descriptor: descriptor }
+                #load_package package_name, descriptor
             else
                 @logger.error "Not loading package #{package_name} because package is invalid"
+                nil
             end
         else
-            puts "not found"
+            puts "No descriptor found"
+            nil
         end
     end
 
@@ -109,6 +142,16 @@ class DiscoveryService < Service
         candidates = @package_db[:installed].values.find_all { |entry| entry[:hash] == pkg_hash }
 
         if candidates.empty?
+            # try resolving dependencies
+            unless package_descriptor['dependencies'].nil?
+                unresolved_deps = package_descriptor['dependencies'].find_all { |dep| @package_db[:installed].keys.member? dep }
+
+                unless unresolved_deps.empty?
+                    @logger.error("Can't resolve dependencies: #{unresolved_deps} for package #{internal_id}")
+                    return false
+                end
+            end
+
             main_file = "#{@watch_dir}/#{internal_id}/#{internal_id}.rb"
             load main_file
 
@@ -134,8 +177,12 @@ class DiscoveryService < Service
                 tx.process_service_queue
                 tx.commit
             end
+
+            true
         else
             @logger.debug "Package #{internal_id} is already installed. Skipping"
+
+            false
         end
     end
 end
