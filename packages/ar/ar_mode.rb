950eeb665c90b1ddacd5291039dae71ac571fb76
Alex Pecsi <alex.pecsi@gmail.com>
2016-02-26 21:08:22 +0000

    ar improved

--- packages/ar/ar_mode.rb
+++ packages/ar/ar_mode.rb
@@ -8,7 +8,17 @@ class ActiveRecordMode < BaseMode
 
     register_command(:exit_mode, "exit", "Exit ActiveRecord browser") { |intp| intp.modes.exit_mode }
     register_command(:select_ns, "select :namespace_id", "Select the current namespace") { |ar, namespace_id| @ns = ar.namespace(namespace_id) }
-    register_command(:show_ns, "show namespaces", "Show registered namespaces") { |intp, ar, out|
+
+    register_command(:show_namespaces, "show namespaces", "Show registered namespaces") { |intp, ar, out|
+        pt = PrinTable.new
+
+        nss = ar.namespaces.map { |ns| ns.id.to_s }
+
+        out.puts pt.print([ "NAMESPACE" ], [ nss ])
+        out.puts "#{nss.length} entries"
+    }
+
+    register_command(:show_models, "show models", "Show registered models") { |intp, ar, out|
         pt = PrinTable.new
 
         # [ { id: id, class_name: Example } ]
